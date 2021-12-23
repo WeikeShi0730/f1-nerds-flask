69834d1d8dd4e60f7d5e62e15a1a76a93ed775ca
Weike Shi <weikeshih@gmail.com>
2021-12-22 23:30:10 -0500

    added flask cahce

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_caching import Cache
 import json
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -6,27 +7,53 @@ import fastf1 as ff1
 from fastf1 import plotting
 from fastf1 import api
 
+config = {
+    "DEBUG": True,  # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300,
+}
+
 app = Flask(__name__)
-api.Cache.enable_cache("./")
+app.config.from_mapping(config)
+cache = Cache(app)
+
+# api.Cache.enable_cache("./")
 
 # session result
 @app.route("/api/year/<year>/weekend/<weekend>/session/<session>")
 def session_result(year, weekend, session):
-    session = ff1.get_session(year, weekend, session)
-    return json.dumps(session.results)
+    cached_session = cache.get(year + "-" + weekend + "-" + session)
+    if cached_session is None:
+        print("NO Cache!!!")
+        session_data = ff1.get_session(year, weekend, session)
+        cache.set(year + "-" + weekend + "-" + session, session_data)
+    else:
+        print("Yes Cache!!!")
+        session_data = cached_session
+
+    return json.dumps(session_data.results)
 
 
 # driver laps
 @app.route("/api/year/<year>/weekend/<weekend>/session/<session>/driver/<driver>")
 def driver_laps(year, weekend, session, driver):
-    laps = ff1.get_session(year, weekend, session).load_laps()
-    driver = laps.pick_driver(driver)
-    laps_time = driver["LapTime"]
-    laps_number = driver["LapNumber"]
-    laps_tire = driver["Compound"]
-    laps_concat = pd.concat([laps_number, laps_time, laps_tire], axis=1)
+    cached_driver_laps = cache.get(year + "-" + weekend + "-" + session + "-" + driver)
+    if cached_driver_laps is None:
+        session_data = cache.get(year + "-" + weekend + "-" + session)
+        if session_data is not None:
+            laps = session_data.load_laps()
+        else:
+            laps = ff1.get_session(year, weekend, session).load_laps()
+        driver_data = laps.pick_driver(driver)
+        laps_time = driver_data["LapTime"]
+        laps_number = driver_data["LapNumber"]
+        laps_tire = driver_data["Compound"]
+        driver_laps_data = pd.concat([laps_number, laps_time, laps_tire], axis=1)
+        cache.set(year + "-" + weekend + "-" + session + "-" + driver, driver_laps_data)
+    else:
+        driver_laps_data = cached_driver_laps
 
-    return laps_concat.to_json()
+    return driver_laps_data.to_json()
 
 
 # driver lap telemetry
@@ -34,37 +61,48 @@ def driver_laps(year, weekend, session, driver):
     "/api/year/<year>/weekend/<weekend>/session/<session>/driver/<driver>/lap/<lap>"
 )
 def driver_lap(year, weekend, session, driver, lap):
-    lap = int(lap)
-    laps = ff1.get_session(year, weekend, session).load_laps(with_telemetry=True)
-    laps_driver = laps.pick_driver(driver)
-    lap_driver = laps_driver.iloc[lap - 1 : lap, :].pick_fastest()
-
-    telemetry_cache = lap_driver.telemetry
-    if not telemetry_cache.empty:
-        lap_telemetry = telemetry_cache
-    else:
-        lap_telemetry = lap_driver.get_car_data()
-    telemetry_speed = lap_telemetry["Speed"]
-    telemetry_throttle = lap_telemetry["Throttle"]
-    telemetry_brake = lap_telemetry["Brake"]
-    telemetry_rpm = lap_telemetry["RPM"]
-    telemetry_gear = lap_telemetry["nGear"]
-    telemetry_drs = lap_telemetry["DRS"]
-    telemetry_concat = pd.concat(
-        [
-            telemetry_speed,
-            telemetry_rpm,
-            telemetry_throttle,
-            telemetry_brake,
-            telemetry_gear,
-            telemetry_drs,
-        ],
-        axis=1,
+    cached_driver_laps = cache.get(
+        year + "-" + weekend + "-" + session + "-" + driver + "-" + lap
     )
-    # with pd.option_context("display.max_rows", None, "display.max_columns", None):
-    #     print(telemetry_speed)
-    return telemetry_concat.to_json()
-    # return lap.to_json()
+    if cached_driver_laps is None:
+        print("NO Cache!!!")
+        lap = int(lap)
+        session_data = cache.get(year + "-" + weekend + "-" + session)
+        if session_data is not None:
+            laps = session_data.load_laps(with_telemetry=True)
+        else:
+            laps = ff1.get_session(year, weekend, session).load_laps(
+                with_telemetry=True
+            )
+        laps_driver = laps.pick_driver(driver)
+        lap_driver = laps_driver.iloc[lap - 1 : lap, :].pick_fastest()
+        lap_telemetry = lap_driver.get_car_data()
+        telemetry_speed = lap_telemetry["Speed"]
+        telemetry_throttle = lap_telemetry["Throttle"]
+        telemetry_brake = lap_telemetry["Brake"]
+        telemetry_rpm = lap_telemetry["RPM"]
+        telemetry_gear = lap_telemetry["nGear"]
+        telemetry_drs = lap_telemetry["DRS"]
+        telemetry_data = pd.concat(
+            [
+                telemetry_speed,
+                telemetry_rpm,
+                telemetry_throttle,
+                telemetry_brake,
+                telemetry_gear,
+                telemetry_drs,
+            ],
+            axis=1,
+        )
+        cache.set(
+            year + "-" + weekend + "-" + session + "-" + driver + "-" + str(lap),
+            telemetry_data,
+        )
+    else:
+        print("Yes Cache!!!")
+        telemetry_data = cached_driver_laps
+
+    return telemetry_data.to_json()
 
 
 if __name__ == "__main__":
