3b6fb9c9c521e6fa20e021d5c36cf7c42bb0dc2d
Weike Shi <weikeshih@gmail.com>
2021-12-24 14:16:03 -0500

    debug redis url

--- worker.py
+++ worker.py
@@ -1,14 +1,14 @@
 import os
 
-from redis import Redis
+import redis
 from rq import Worker, Queue, Connection
 
 listen = ["high", "default", "low"]
 
-redis_url = os.getenv("REDISTOGO_URL")
+redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
 
-# conn = redis.from_url(redis_url)
-conn = Redis()
+conn = redis.from_url(redis_url)
+# conn = Redis()
 
 if __name__ == "__main__":
     with Connection(conn):
